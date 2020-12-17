814bd49964a64319d3347aba0e16781442e06bc4
Danil Platonov <danil.platonov@alumni.ubc.ca>
2020-12-17 23:00:09 +0000

    Seems to work

--- Dockerfile
+++ Dockerfile
@@ -28,6 +28,12 @@ RUN apt-get update && apt-get install -yq --no-install-recommends \
     screen \
     && apt-get clean && rm -rf /var/lib/apt/lists/*
 
+COPY requirements.txt .
+RUN pip3 install -r requirements.txt
+RUN jupyter serverextension enable --sys-prefix jupyter_server_proxy
+
+COPY jupyter_notebook_config.py /etc/jupyter/
+
 USER $NB_UID
 
-CMD ["/usr/bin/change-user.sh"]
+#CMD ["/usr/bin/change-user.sh"]
--- jh-entrypoint.sh
+++ jh-entrypoint.sh
@@ -2,4 +2,4 @@
 
 #su $NB_USER -c python3 -m jhsingle_native_proxy.main -- /usr/bin/code-server --auth none --bind-addr 0.0.0.0:{port} .
 #python3 -m jhsingle_native_proxy.main -- su $NB_USER -c /usr/bin/code-server --auth none --bind-addr 0.0.0.0:{port} .
-python3 -m jhsingle_native_proxy.main -- su $NB_USER -c '/usr/bin/code-server --auth none --bind-addr 0.0.0.0:{port} .'
+#python3 -m jhsingle_native_proxy.main -- su $NB_USER -c '/usr/bin/code-server --auth none --bind-addr 0.0.0.0:{port} .'
